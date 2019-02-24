b75de57230ee75a8df00dfea4c74cb0fd34f633d
arasosman <osmanaras50@gmail.com>
2019-02-24 13:19:10 +0300

    first poll

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Merhaba Havuz. İlk NodeJs Proje' });
 });
 
 module.exports = router;
